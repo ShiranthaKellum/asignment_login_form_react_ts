57b97714bfbeaa49eecb44274599fb63c4ce261f
Shirantha Kellum <skellumjayasooriya@gmail.com>
2022-11-17 23:39:48 +0530

    Feedback changes

--- src/Pages/homePage.tsx
+++ src/Pages/homePage.tsx
@@ -1,20 +1,16 @@
 import { useLocation, useNavigate } from "react-router-dom"
 
-export default function HomePage () {
+export default function HomePage() {
     const navigate = useNavigate();
     const location = useLocation();
 
-    const logout = () => {
-        navigate('/');
-
-    }
+    const logout = () => navigate('/');
 
     return (
         <div>
             <h1>Home Page</h1>
             <h2>Welcome {location.state.username}</h2>
-        <input type="button" value="Logout" onClick={logout}/>
-        
-        </div>       
+            <input type="button" value="Logout" onClick={logout} />
+        </div>
     )
 }
--- src/Pages/loginPage.tsx
+++ src/Pages/loginPage.tsx
@@ -2,71 +2,51 @@ import { ChangeEvent, FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 export default function LoginPage() {
-    const initData = {
+    const initialLoginData = {
         username: '',
         password: '',
-
     }
     // data set
-    const [data, setData] = useState(initData);
+    const [data, setData] = useState<{ username: string; password: string; }>(initialLoginData);
+    // errors
+    const [userNameError, setUserNameError] = useState<boolean>(false);
+    const [passwordError, setPasswordError] = useState<boolean>(false);
     const { username, password } = data;
 
     const navigate = useNavigate();
-    // errors
-    const [userNameError, setUserNameError] = useState(false);
-    const [passwordError, setPasswordError] = useState(false);
-
+    
     const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value.length);
         setData((prevState) => ({
             ...prevState,
             [e.target.name]: e.target.value,
-
         }));
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (username === '') {
-            setUserNameError(true);
-
-        } else {
-            if (password === '') {
-                setPasswordError(true);
-
-            } else {
-                if (username === 'admin' && password === 'password') {
-                    navigate('/homepage', { state: { username: username } });
-
-                }
-            }
+        if (username === '') setUserNameError(true);
+        if (password === '') setPasswordError(true);
+        if (username === 'admin' && password === 'password') {
+            navigate('/homepage', { state: { username: username } });
         }
     }
+
     return (
         <div>
             <h1>Login Page</h1>
             <h2>Login Form</h2>
-            <form onSubmit={e => { handleSubmit(e) }}>
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="username">Username</label>
                 <input
                     type="text"
                     placeholder="username"
                     name="username"
                     value={username}
-                    onChange={e => onChange(e)}
-
+                    onChange={onChange}
                 />
                 <br />
-                {userNameError ?
-                    <label
-                        style={{
-                            color: 'red',
-
-                        }}
-
-                    >Username can not be empty!
-
-                    </label> : ''
-                }
+                {userNameError && <label style={{color: 'red',}}>Username can not be empty!</label>}
                 <br />
                 <label htmlFor="password">Password</label>
                 <input
@@ -74,21 +54,10 @@ export default function LoginPage() {
                     placeholder="password"
                     name="password"
                     value={password}
-                    onChange={e => onChange(e)}
-
+                    onChange={onChange}
                 />
                 <br />
-                {passwordError ?
-                    <label
-                        style={{
-                            color: 'red',
-
-                        }}
-
-                    >Password can not be empty!
-
-                    </label> : ''
-                }
+                {passwordError && <label style={{color: 'red',}}>Password can not be empty!</label>}
                 <br />
                 <input type="submit" value="Login" />
             </form>
